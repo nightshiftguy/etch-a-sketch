15781c6ccfbca0a42de18dd398b19d006a4a2397
Jan <jas.gol@poczta.onet.pl>
2024-08-11 16:08:21 +0200

    Add function to create grid

---
+++ script.js
@@ -0,0 +1,16 @@
+let gridContainer = document.querySelector("#gridContainer");
+let squares = [];
+
+function createGrid(squaresPerSide){
+    let squareAmount = squaresPerSide * squaresPerSide;
+    sideLength = 500 / squaresPerSide;
+    
+    for(let i = 0; i<squareAmount; i++){
+        squares.push(document.createElement("div"));
+        squares[i].setAttribute("class", "gridSquare");
+        squares[i].setAttribute("style","width: "+sideLength+"px; height: "+sideLength+"px;");
+        gridContainer.appendChild(squares[i]);
+    }
+}
+
+createGrid(4);
